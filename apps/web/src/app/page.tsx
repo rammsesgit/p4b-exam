ab1ff2db07aaa16d80b9b3be25060f8ceac128ef
Isar Ramses <rammses_@live.com.mx>
2024-02-23 03:19:34 -0600

    Add profile component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,42 @@
 'use client'
 
-// import './globals.css'
+import Profile from '@repo/ui/profile'
 
-import { useEffect, useState } from 'react'
+// TEST:
+const profiles = [
+  {
+    id: 1,
+    name: 'Sarah',
+    img_url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?fit=facearea&facepad=4&w=198&h=198',
+  },
+  {
+    id: 2,
+    name: 'John',
+    img_url: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?fit=facearea&facepad=4&w=198&h=198',
+  },
+  {
+    id: 3,
+    name: 'Bess',
+    img_url: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?fit=facearea&facepad=4&w=198&h=198',
+  },
+]
 
 export default function Blogs() {
-  useEffect(() => {
-    // TODO: Get the profiles
-  }, [])
-
   return (
-    <>
-      <h1>Blog</h1>
-    </>
+    <div className='flex justify-center items-center flex-col mt-[20vh] gap-20'>
+      <h1 className='text-center'>Choose your profile</h1>
+
+      <section className='grid grid-cols-2 sm:grid-cols-4 gap-6 max-w-2xl mx-auto'>
+        {profiles.map(p => (
+          <button key={p.id}>
+            <Profile {...p} />
+          </button>
+        ))}
+
+        <button>
+          <Profile name='Guest' />
+        </button>
+      </section>
+    </div>
   )
 }
