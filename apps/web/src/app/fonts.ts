83681a061dd2b8621c00067b2bcb78b54424ed16
Isar Ramses <rammses_@live.com.mx>
2024-02-22 23:56:04 -0600

    Add fonts

---
+++ apps/web/src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Jura } from 'next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
+export const jura = Jura({
+  subsets: ['latin'],
+  variable: '--font-jura',
+  display: 'swap',
+})
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,21 +1,21 @@
-import { Inter } from 'next/font/google'
+import '@repo/ui/styles.css'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import { inter, jura } from './fonts'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <head>
         <title>Blog</title>
-        <link rel='icon' href='img/favicon.png' />
+        <link rel='icon' href='/img/favicon.png' />
 
-        <meta name='title' content='Blog' />
-        <meta itemProp='name' content='Blog' />
+        <meta name='title' content='Blog | P4B exam' />
+        <meta itemProp='name' content='Blogs' />
         <meta name='description' content='React developer exam' />
         <meta itemProp='description' content='React developer exam' />
       </head>
-      <body className={inter.className}>
+      <body className={`${jura.variable} ${inter.variable}`}>
         <main className='main-content'>
           <span className='absolute pointer-events-none mix-blend-normal will-change-[filter] inset-x-0 rounded-full opacity-10 -top-[50vh] h-screen blur-[75px] bg-glow-conic' />
           {children}
